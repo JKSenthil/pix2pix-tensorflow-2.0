d0c941c24fe8162dd06613ed21fd2bad6dc885b0
Yuan Gao <yuan_gao@brown.edu>
2019-12-03 21:36:37 -0500

    implemented a UnetGenerator

--- src/generator.py
+++ src/generator.py
@@ -46,9 +46,9 @@ class Decoder(Layer):
     def __init__(self, ngf=64, **kwargs):
        super(Encoder, self).__init__(**kwargs)
        used_in_encoder = True
-       self.conv_1 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder, True)
-       self.conv_2 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder, True)
-       self.conv_3 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder, True)
+       self.conv_1 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder, use_dropout=True)
+       self.conv_2 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder, use_dropout=True)
+       self.conv_3 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder, use_dropout=True)
        self.conv_4 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder)
        self.conv_5 = Conv_BatchNorm_ReLU(ngf * 4, used_in_encoder)
        self.conv_6 = Conv_BatchNorm_ReLU(ngf * 2, used_in_encoder)
@@ -60,9 +60,6 @@ class Decoder(Layer):
         return self.conv_7(self.conv_6(self.conv_5(temp)))
 
 class AutoEncoder(tf.keras.Model):
-    """
-    Class for the pix2pix generator.
-    """
     def __init__(self):
         """
         Definition of Generator model.
@@ -70,7 +67,7 @@ class AutoEncoder(tf.keras.Model):
         final dense layer, etc.
         """
         super(AutoEncoder, self).__init__()
-        #TODO Add series of layers using tf.keras.layers
+        # Add series of layers using tf.keras.layers
         self.encoder = Encoder(input_shape=(256, 256, 3))
         self.decoder = Decoder()
 
@@ -90,52 +87,52 @@ class AutoEncoder(tf.keras.Model):
         pass
 
 class UnetGenerator(tf.keras.Model):
-    def __init__(self, ngf=64):
-        super(UnetGenerator, self).__init__()
-        self.unet = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_shape=(256, 256, 3))
-        for _ in range(4):
-            self.unet = UnetSkipConnectionBlock(ngf * 8, ngf * 16, submodule=self.unet)
-        self.unet = UnetSkipConnectionBlock(ngf * 4, ngf * 8, submodule=self.unet)
-        self.unet = UnetSkipConnectionBlock(ngf * 2, ngf * 4, submodule=self.unet)
-        self.unet = UnetSkipConnectionBlock(ngf * 1, ngf * 2, submodule=self.unet, outermost=True)
+    """
+    Class for the pix2pix generator.
+    """
+    def __init__(self, ngf=64, lambda_=0.01, input_nc, output_nc):
+        super(AutoEncoder, self).__init__()
+        self.lambda_ = lambda_
+        self.conv_1 = Conv_BatchNorm_ReLU(ngf * 1, used_in_encoder=True, input_shape=(256, 256, input_nc))
+        self.conv_2 = Conv_BatchNorm_ReLU(ngf * 2, used_in_encoder=True)
+        self.conv_3 = Conv_BatchNorm_ReLU(ngf * 4, used_in_encoder=True)
+        self.conv_4 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=True)
+        self.conv_5 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=True)
+        self.conv_6 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=True)
+        self.conv_7 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=True)
+        self.conv_8 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=True)
+
+        self.deconv_8 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=False, use_dropout=True)
+        self.deconv_7 = Conv_BatchNorm_ReLU(ngf * 16, used_in_encoder=False, use_dropout=True)
+        self.deconv_6 = Conv_BatchNorm_ReLU(ngf * 16, used_in_encoder=False, use_dropout=True)
+        self.deconv_5 = Conv_BatchNorm_ReLU(ngf * 16, used_in_encoder=False)
+        self.deconv_4 = Conv_BatchNorm_ReLU(ngf * 16, used_in_encoder=False)
+        self.deconv_3 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=False)
+        self.deconv_2 = Conv_BatchNorm_ReLU(ngf * 4, used_in_encoder=False)
+        self.deconv_1 = Conv_BatchNorm_ReLU(output_nc, used_in_encoder=False)
 
     @tf.function
     def call(self, images):
-        return self.unet(images)
+        output_1 = self.conv_1(images)
+        output_2 = self.conv_2(output_1)
+        output_3 = self.conv_3(output_2)
+        output_4 = self.conv_4(output_3)
+        output_5 = self.conv_5(output_4)
+        output_6 = self.conv_6(output_5)
+        output_7 = self.conv_7(output_6)
+        output_8 = self.conv_7(output_7)
+
+        result = self.deconv_8(output_8)
+        result = self.deconv_7(tf.concat([output_7, result], -1))
+        result = self.deconv_6(tf.concat([output_6, result], -1))
+        result = self.deconv_5(tf.concat([output_5, result], -1))
+        result = self.deconv_4(tf.concat([output_4, result], -1))
+        result = self.deconv_3(tf.concat([output_3, result], -1))
+        result = self.deconv_2(tf.concat([output_2, result], -1))
+        return self.deconv_1(tf.concat([output_1, result], -1))
 
     @tf.function
-    def loss_function(self,real_outputs,generated_outputs):
-        pass
-
-# Code adapted from:
-# https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
-class UnetSkipConnectionBlock(Layer):
-    def __init__(self, inner_nc, outer_nc, submodule=None, outermost=False, **kwargs):
-        super(MyLayer, self).__init__(**kwargs)
-        innermost = submodule is None
-        assert(not (innermost and outermost))
-        self.outermost = outermost
-        self.submodule = submodule if not innermost else Lambda(lambda x: x)
-        
-        self.downrelu = Lambda(lambda x: x)
-        self.downconv = Conv2D(inner_nc, kernel_size=(4, 4), strides=(2, 2), padding="same")
-        self.downnorm = Lambda(lambda x: x)
-
-        self.uprelu = ReLU()
-        self.upconv = Conv2dTranspose(outer_nc, kernel_size=(4, 4), strides=(2, 2), padding="same")
-        self.upnorm = BatchNormalization()
-
-        if outermost:
-            self.upnorm = tf.math.tanh
-        else:
-            self.downrelu = LeakyReLU(0.2)
-            if not innermost:
-                self.downnorm = BatchNormalization()
-        
-    @tf.function
-    def call(self, inputs):
-        down = self.downnorm(self.downconv(self.downrelu(inputs)))
-        outputs = self.upnorm(self.upconv(self.uprelu(self.submodule(down))))
-        if self.outermost:
-            return outputs
-        return tf.concat((inputs, outputs), -1)
+    def loss_function(self, discriminator, generated_outputs, ground_truth):
+        disc_fake_outputs = discriminator(generated_outputs)
+        return tf.reduce_mean(tf.keras.losses.binary_crossentropy(y_true=tf.zeros_like(logits_fake), y_pred=disc_fake_outputs)) + \
+            tf.math.scalar_mul(tf.constant(self.lambda_), tf.norm(generated_outputs - ground_truth, 1))
