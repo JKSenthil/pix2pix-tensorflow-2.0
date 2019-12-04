217e17f2a7febf8052af4f69f5973eee14e7d0e3
Yuan Gao <yuan_gao@brown.edu>
2019-12-04 16:48:28 -0500

    Modified the architecture of UnetGenerator

--- src/generator.py
+++ src/generator.py
@@ -21,29 +21,30 @@ class Conv_BatchNorm_RelU(Layer):
     @tf.function
     def call(self, inputs):
         return self.relu(self.dropout(self.batchnorm(self.conv(inputs))))
-
+# Code adapted from: 
+# https://github.com/phillipi/pix2pix/blob/master/models.lua
 class Encoder(Layer):
     def __init__(self, ngf=64, **kwargs):
        super(Encoder, self).__init__(**kwargs)
        used_in_encoder = True
        self.conv_1 = Conv2D(
            filters=ngf, kernel_size=(4, 4), strides=(2, 2), padding="same")
-       self.relu_1 = LeakyReLU(0.2)
        self.conv_2 = Conv_BatchNorm_ReLU(ngf * 2, used_in_encoder)
        self.conv_3 = Conv_BatchNorm_ReLU(ngf * 4, used_in_encoder)
        self.conv_4 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder)
        self.conv_5 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder)
        self.conv_6 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder)
        self.conv_7 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder)
-       self.conv_8 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder)
+       self.conv_8 = Conv2D(
+           filters=ngf * 8, kernel_size=(4, 4), strides=(2, 2), padding="same")
 
     @tf.function
     def call(self, inputs):
-        temp = self.conv_4(self.conv_3(self.conv_2(self.relu_1(self.conv_1(inputs)))))
-        return self.conv_8(self.conv_7(self.conv_6(self.conv_5(temp))))
+        temp = self.conv_4(self.conv_3(self.conv_2(tf.nn.leaky_relu(self.conv_1(inputs), 0.2))))
+        return tf.nn.relu(self.conv_8(self.conv_7(self.conv_6(self.conv_5(temp)))))
 
 class Decoder(Layer):
-    def __init__(self, ngf=64, **kwargs):
+    def __init__(self, output_nc, ngf=64, **kwargs):
        super(Encoder, self).__init__(**kwargs)
        used_in_encoder = True
        self.conv_1 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder, use_dropout=True)
@@ -53,14 +54,16 @@ class Decoder(Layer):
        self.conv_5 = Conv_BatchNorm_ReLU(ngf * 4, used_in_encoder)
        self.conv_6 = Conv_BatchNorm_ReLU(ngf * 2, used_in_encoder)
        self.conv_7 = Conv_BatchNorm_ReLU(ngf * 1, used_in_encoder)
+       self.conv_8 = Conv2DTranspose(
+            filters=output_nc, kernel_size=(4, 4), strides=(2, 2), padding="same")
 
     @tf.function
     def call(self, inputs):
         temp = self.conv_4(self.conv_3(self.conv_2(self.conv_1(inputs))))
-        return self.conv_7(self.conv_6(self.conv_5(temp)))
+        return tf.math.tanh(self.conv_8(self.conv_7(self.conv_6(self.conv_5(temp)))))
 
 class AutoEncoder(tf.keras.Model):
-    def __init__(self):
+    def __init__(self, input_nc, output_nc):
         """
         Definition of Generator model.
         Has a number of layers, including several convolutional layers, batch normalization,
@@ -68,8 +71,8 @@ class AutoEncoder(tf.keras.Model):
         """
         super(AutoEncoder, self).__init__()
         # Add series of layers using tf.keras.layers
-        self.encoder = Encoder(input_shape=(256, 256, 3))
-        self.decoder = Decoder()
+        self.encoder = Encoder(input_shape=(256, 256, input_nc))
+        self.decoder = Decoder(output_nc)
 
     @tf.function
     def call(self, images):
@@ -91,45 +94,48 @@ class UnetGenerator(tf.keras.Model):
     Class for the pix2pix generator.
     """
     def __init__(self, ngf=64, lambda_=0.01, input_nc, output_nc):
-        super(AutoEncoder, self).__init__()
+        super(UnetGenerator, self).__init__()
         self.lambda_ = lambda_
-        self.conv_1 = Conv_BatchNorm_ReLU(ngf * 1, used_in_encoder=True, input_shape=(256, 256, input_nc))
+        self.conv_1 = Conv2D(
+           filters=ngf, kernel_size=(4, 4), strides=(2, 2), padding="same")
         self.conv_2 = Conv_BatchNorm_ReLU(ngf * 2, used_in_encoder=True)
         self.conv_3 = Conv_BatchNorm_ReLU(ngf * 4, used_in_encoder=True)
         self.conv_4 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=True)
         self.conv_5 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=True)
         self.conv_6 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=True)
         self.conv_7 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=True)
-        self.conv_8 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=True)
+        self.conv_8 = Conv2D(
+           filters=ngf * 8, kernel_size=(4, 4), strides=(2, 2), padding="same")
 
         self.deconv_8 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=False, use_dropout=True)
-        self.deconv_7 = Conv_BatchNorm_ReLU(ngf * 16, used_in_encoder=False, use_dropout=True)
-        self.deconv_6 = Conv_BatchNorm_ReLU(ngf * 16, used_in_encoder=False, use_dropout=True)
-        self.deconv_5 = Conv_BatchNorm_ReLU(ngf * 16, used_in_encoder=False)
-        self.deconv_4 = Conv_BatchNorm_ReLU(ngf * 16, used_in_encoder=False)
-        self.deconv_3 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=False)
-        self.deconv_2 = Conv_BatchNorm_ReLU(ngf * 4, used_in_encoder=False)
-        self.deconv_1 = Conv_BatchNorm_ReLU(output_nc, used_in_encoder=False)
+        self.deconv_7 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=False, use_dropout=True)
+        self.deconv_6 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=False, use_dropout=True)
+        self.deconv_5 = Conv_BatchNorm_ReLU(ngf * 8, used_in_encoder=False)
+        self.deconv_4 = Conv_BatchNorm_ReLU(ngf * 4, used_in_encoder=False)
+        self.deconv_3 = Conv_BatchNorm_ReLU(ngf * 2, used_in_encoder=False)
+        self.deconv_2 = Conv_BatchNorm_ReLU(ngf * 1, used_in_encoder=False)
+        self.deconv_1 = Conv2DTranspose(
+            filters=output_nc, kernel_size=(4, 4), strides=(2, 2), padding="same")
 
     @tf.function
     def call(self, images):
-        output_1 = self.conv_1(images)
-        output_2 = self.conv_2(output_1)
-        output_3 = self.conv_3(output_2)
-        output_4 = self.conv_4(output_3)
-        output_5 = self.conv_5(output_4)
-        output_6 = self.conv_6(output_5)
-        output_7 = self.conv_7(output_6)
-        output_8 = self.conv_7(output_7)
+        encoder_output_1 = tf.nn.leaky_relu(self.conv_1(images), 0.2)
+        encoder_output_2 = self.conv_2(encoder_output_1)
+        encoder_output_3 = self.conv_3(encoder_output_2)
+        encoder_output_4 = self.conv_4(encoder_output_3)
+        encoder_output_5 = self.conv_5(encoder_output_4)
+        encoder_output_6 = self.conv_6(encoder_output_5)
+        encoder_output_7 = self.conv_7(encoder_output_6)
+        encoder_output_8 = tf.nn.relu(self.conv_8(encoder_output_7))
 
-        result = self.deconv_8(output_8)
-        result = self.deconv_7(tf.concat([output_7, result], -1))
-        result = self.deconv_6(tf.concat([output_6, result], -1))
-        result = self.deconv_5(tf.concat([output_5, result], -1))
-        result = self.deconv_4(tf.concat([output_4, result], -1))
-        result = self.deconv_3(tf.concat([output_3, result], -1))
-        result = self.deconv_2(tf.concat([output_2, result], -1))
-        return self.deconv_1(tf.concat([output_1, result], -1))
+        decoder_output = encoder_output_8
+        for deconv, encoder_output in zip(
+            [self.deconv_8, self.deconv_7, self.deconv_6, self.deconv_5, self.deconv_4, self.deconv_3, self.deconv_2],
+            [encoder_output_7, encoder_output_6, encoder_output_5, encoder_output_4, encoder_output_3, encoder_output_2, encoder_output_1]
+        ):
+            _decoder_output = deconv(decoder_output)
+            decoder_output = tf.concat((_decoder_output, encoder_output), -1)
+        return tf.math.tanh(self.deconv_1(decoder_output))
 
     @tf.function
     def loss_function(self, discriminator, generated_outputs, ground_truth):
